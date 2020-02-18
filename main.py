20ccd431c0cec5ad5c70551b941d7c289c3073c9
Tom <thomas.verin@sonarsource.com>
2020-02-18 15:41:28 +0100

    Use func

--- main.py
+++ main.py
@@ -3,17 +3,16 @@ import os
 import requests
 import json
 
+def show_output(output):
+  print(f"::set-output name=myOutput::{output}")
 
 def main():
     my_input = os.environ["INPUT_MYINPUT"]
     my_output = f"Testing: {my_input}"
-    print(f"::set-output name=myOutput::{my_output}")
-    #show_output(my_output)
-
+    #print(f"::set-output name=myOutput::{my_output}")
+    show_output(my_output)
 
 if __name__ == "__main__":
     main()
 
-def show_output(output):
-  print(f"::set-output name=myOutput::{output}")
   
