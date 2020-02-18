fad99afa79cc6c76fe0593e16de875d5bcb526f0
Tom <thomas.verin@sonarsource.com>
2020-02-18 15:09:28 +0100

    Fix

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ import json
 def main():
     my_input = os.environ["INPUT_MYINPUT"]
     my_output = f"Testing: {my_input}"
-    show_output(my_output)
+    print(f"::set-output name=myOutput::{my_output}")
+    #show_output(my_output)
 
 
 if __name__ == "__main__":
