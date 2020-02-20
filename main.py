d50ee3f0cba93f54b2600aee11d4bc59050f9176
Christophe Zürn <36889251+christophe-zurn-sonarsource@users.noreply.github.com>
2020-02-20 17:29:13 +0100

    Add releasability check to action

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import sys
 import os
 import requests
-import json
 
 githup_api_url="https://api.github.com"
 
@@ -26,19 +25,53 @@ def revoke_release(repo):
   r=requests.patch(url, json=payload, headers=headers)
   return r.json()
   
-def main():
+def do_release(repo, sha1, headers):
     function_url="https://us-central1-language-team.cloudfunctions.net/release"
+    url=f"{function_url}/{repo}/{sha1}/"
+    return requests.get(url, headers=headers)
+
+def check_releasability(repo, sha1, headers):
+    function_url="https://us-central1-language-team.cloudfunctions.net/releasability_check"
+    url=f"{function_url}/{repo}/{sha1}/"
+    return requests.get(url, headers=headers)
+
+def print_releasability_details(data):
+    show_output(f"RELEASABILITY: {data.get('state')}")
+    checks=data.get('checks', [])
+    for check in checks:
+        msg=check.get('message', '')
+        if msg:
+            msg=f": {msg}"
+        show_output(f"{check.get('name')} - {check.get('state')}{msg}")
+
+def releasability_passed(response):
+    if response.status_code == 200:
+        data=response.json()
+        print_releasability_details(data)
+        return data.get('state') == 'PASSED'
+    return False
+
+def abort_release(repo):
+    show_output("Aborting release")
+    revoke_release(repo)
+    sys.exit(1)
+
+def main():
     sha1=os.environ["GITHUB_SHA"]
     repo=os.environ["GITHUB_REPOSITORY"]
-    url = f"{function_url}/{repo}/{sha1}/"
-    GITHUB_TOKEN=os.environ["GITHUB_TOKEN"]
-    headers = {'Authorization': f"token {GITHUB_TOKEN}"}
-    r = requests.get(url, headers=headers)
-    if r.status_code == 200:      
-      show_output(url)
-    else:
-      revoke_release(repo)
+    github_token=os.environ["GITHUB_TOKEN"]
+    headers={'Authorization': f"token {github_token}"}
 
+    r=check_releasability(repo, sha1, headers)
+    if releasability_passed(r):
+        r=do_release(repo, sha1, headers)
+        if r.status_code == 200:
+            show_output(f"{repo}:{sha1} RELEASED")
+        else:
+            show_output("Unexpected exception occurred while calling release cloud function")
+            abort_release(repo)
+    else:
+        abort_release(repo)
     
 
 if __name__ == "__main__":
