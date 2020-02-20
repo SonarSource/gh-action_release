b2616043271112b0268683a65f59d28d45b9bf2b
Tom <thomas.verin@sonarsource.com>
2020-02-20 15:42:18 +0100

    put release in draft if release fail

--- main.py
+++ main.py
@@ -3,9 +3,29 @@ import os
 import requests
 import json
 
+githup_api_url="https://api.github.com"
+
 def show_output(output):
   print(f"::set-output name=myOutput::{output}")
 
+def get_release_info(repo):
+  tag=os.environ["GITHUB_REF"]
+  url=f"{githup_api_url}/repos/{repo}/releases/tags/{tag}"
+  GITHUB_TOKEN=os.environ["GITHUB_TOKEN"]
+  headers = {'Authorization': f"token {GITHUB_TOKEN}"}
+  r = requests.get(url, headers=headers)  
+  return r.json()
+
+def revoke_release(repo):
+  release_info=get_release_info(repo)
+  release_id=release_info['id']
+  url=f"{githup_api_url}/repos/{repo}/releases/{release_id}"
+  GITHUB_TOKEN=os.environ["GITHUB_TOKEN"]
+  headers = {'Authorization': f"token {GITHUB_TOKEN}"}
+  payload = {'draft': True}
+  r=requests.patch(url, json=payload, headers=headers)
+  return r.json()
+  
 def main():
     function_url="https://us-central1-language-team.cloudfunctions.net/release"
     sha1=os.environ["GITHUB_SHA"]
@@ -14,8 +34,12 @@ def main():
     GITHUB_TOKEN=os.environ["GITHUB_TOKEN"]
     headers = {'Authorization': f"token {GITHUB_TOKEN}"}
     r = requests.get(url, headers=headers)
+    if r.status_code == 200:      
+      show_output(url)
+    else:
+      revoke_release(repo)
 
-    show_output(url)
+    
 
 if __name__ == "__main__":
     main()
--- test_release.py
+++ test_release.py
@@ -1,4 +1,19 @@
 from main import show_output
+from main import get_release_info,revoke_release
+from unittest.mock import patch
+
+def test_get_release_id():
+  with patch.dict('os.environ', {'GITHUB_REF': '10.3'}):
+    release=get_release_info("SonarSource/sonar-dummy")
+    print(release['name'])
+    assert "23833527"==str(release['id'])
+
+
+def test_revoke_release():
+  with patch.dict('os.environ', {'GITHUB_REF': '10.3'}):
+    revoke=revoke_release("SonarSource/sonar-dummy")
+    print(str(revoke))
+    assert revoke['draft']
 
 def test_show_output():
   show_output("bla bla")
