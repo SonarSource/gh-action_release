0da177407b25a77f79e1109caf927ffc00912467
Christophe Zürn <36889251+christophe-zurn-sonarsource@users.noreply.github.com>
2020-02-21 10:01:43 +0100

    Update test_release.py

--- test_release.py
+++ test_release.py
@@ -1,19 +1,19 @@
 from main import show_output
-from main import get_release_id,revoke_release
+from main import get_release_id, revoke_release
 from unittest.mock import patch
 
+
 def test_get_release_id():
-  with patch.dict('os.environ', {'GITHUB_REF': '10.3'}):
-    release=get_release_id("SonarSource/sonar-dummy")
-    print(release['name'])
-    assert "23833527"==str(release['id'])
+    release_id = get_release_id("SonarSource/sonar-dummy", "10.3")
+    assert "23844035" == str(release_id)
 
 
 def test_revoke_release():
-  with patch.dict('os.environ', {'GITHUB_REF': '10.3'}):
-    revoke=revoke_release("SonarSource/sonar-dummy")
-    print(str(revoke))
-    assert revoke['draft']
+    with patch.dict('os.environ', {'GITHUB_REF': '10.3'}):
+        revoke = revoke_release("SonarSource/sonar-dummy")
+        print(str(revoke))
+        assert revoke['draft']
+
 
 def test_show_output():
-  show_output("bla bla")
+    show_output("bla bla")
