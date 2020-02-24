d522545caf79275015cf4efc5526ef036c278c7f
Tom <thomas.verin@sonarsource.com>
2020-02-24 10:27:04 +0100

    Add version tester but do not implement because it doesn't work with milestones

--- test_release.py
+++ test_release.py
@@ -1,6 +1,6 @@
 from main import get_release_info, revoke_release
 from unittest.mock import patch
-
+import re
 
 def test_get_release_id():
     release_info = get_release_info("SonarSource/sonar-dummy", "10.3")
@@ -17,3 +17,18 @@ def test_revoke_release():
         revoke = revoke_release("SonarSource/sonar-dummy")
         print(str(revoke))
         assert revoke['draft']
+
+def test_version():
+  #version_regexp = re.compile(r'^(\d*)\.(\d*)\.(\d*)\.(\d*)$')
+  version_regexp = re.compile(r'^\d+(\.\d+){2,3}$')
+  versions = ['dsvfsdf',
+              '1.a.3',
+              '1.2.4',
+              '10.0.0.1234',
+              '1.4..5.6',
+              '0.3.1',
+              'a.b.c.d',
+              '1,2,3']
+  for i, sample in enumerate(versions):
+      #print('Sample[%d]: %s' % (i, True if version_regexp.match(sample) else False))
+      print(f"{versions[i]} {True if version_regexp.match(sample) else False}")
