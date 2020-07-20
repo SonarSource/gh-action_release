a559af4709c63da6b704514fa32132608646a0a2
Malena Ebert <malena.ebert@sonarsource.com>
2020-07-20 12:44:47 +0200

    update

--- utils/github.py
+++ utils/github.py
@@ -1,35 +1,21 @@
 import os
-
+import json
 import requests
 
 githup_api_url = "https://api.github.com"
 github_token = os.environ.get('GITHUB_TOKEN', 'no github token in env')
+github_event_path = os.environ.get('GITHUB_EVENT_PATH')
 attach_to_github_release = None
 run_rules_cov = None
 
 
-def get_release_info(release_request, version):
-  url = f"{githup_api_url}/repos/{release_request.org}/{release_request.project}/releases"
-  headers = {'Authorization': f"token {github_token}"}
-  r = requests.get(url, headers=headers)
-  releases = r.json()
-  for release in releases:
-    if not isinstance(release, str) and release.get('tag_name') == version:
-      return release
-  print(f"::error No release info found for tag '{version}'.\nReleases: {releases}")
-  return None
-
-
 def get_release_info(repo: str, version):
-  url = f"{githup_api_url}/repos/{repo}/releases"
-  headers = {'Authorization': f"token {github_token}"}
-  r = requests.get(url, headers=headers)
-  releases = r.json()
-  for release in releases:
-    if not isinstance(release, str) and release.get('tag_name') == version:
-      return release
-  print(f"::error No release info found for tag '{version}'.\nReleases: {releases}")
-  return None
+  with open(github_event_path) as file:
+    data = json.load(file)
+    if data["release"].get('tag_name') == version:
+      return data["release"]
+    else:
+      return None
 
 
 def attach_asset_to_github_release(release_info, file_path, filename):
@@ -53,3 +39,15 @@ def revoke_release(repo, version):
   url = f"{githup_api_url}/repos/{repo}/git/refs/tags/{version}"
   requests.delete(url, headers=headers)
   return r.json()
+
+
+def current_branch():
+  with open(github_event_path) as file:
+    data = json.load(file)
+    if 'release' not in data:
+      print(f"::error Could not get release object of github event")
+      return None
+    if 'target_commitish' not in data['release']:
+      print(f"::error Could not get the branch name of github event")
+      return None
+    return data['release']['target_commitish']
