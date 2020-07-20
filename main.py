9c82f3e7b06a5638a119da04a97c4f48517786ba
Malena Ebert <malena.ebert@sonarsource.com>
2020-07-20 11:15:17 +0200

    Use 2 spaces indentation

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import json
-import re
-import sys
 import os
-import traceback
+import sys
 
 import steps.distribute as distributeStep
-from steps import release, relesability
-from steps.distribute import ReleaseRequest
+from steps import release
+from utils.ReleaseRequest import ReleaseRequest
 from utils.artifactory import Artifactory
 from utils.github import revoke_release, get_release_info
 
@@ -21,69 +19,66 @@ artifactory_apikey = os.environ.get('ARTIFACTORY_API_KEY', 'no api key in env')
 
 
 def set_releasability_output(output):
-    print(f"::set-output name=releasability::{output}")
+  print(f"::set-output name=releasability::{output}")
 
 
 def set_release_output(function, output):
-    print(f"::set-output name={function}::{output}")
+  print(f"::set-output name={function}::{output}")
 
 
 def abort_release(repo, version):
-    print(f"::error  Aborting release")
-    revoke_release(repo, version)
-    sys.exit(1)
+  print(f"::error  Aborting release")
+  revoke_release(repo, version)
+  sys.exit(1)
 
 
 def current_branch():
-    with open(github_event_path) as file:
-        data = json.load(file)
-        if 'release' not in data:
-            print(f"::error Could not get release object of github event")
-            return None
-        if 'target_commitish' not in data['release']:
-            print(f"::error Could not get the branch name of github event")
-            return None
-        possible_branch_name = data['release']['target_commitish']
-        if re.compile("^([a-f0-9]{40})$").match(possible_branch_name):
-            return None
-        return possible_branch_name
+  with open(github_event_path) as file:
+    data = json.load(file)
+    if 'release' not in data:
+      print(f"::error Could not get release object of github event")
+      return None
+    if 'target_commitish' not in data['release']:
+      print(f"::error Could not get the branch name of github event")
+      return None
+    return data['release']['target_commitish']
 
 
 def main():
-    repo = os.environ["GITHUB_REPOSITORY"]
-    organisation, project = repo.split("/")
-    tag = os.environ["GITHUB_REF"]
-    version = tag.replace('refs/tags/', '', 1)
-    # tag shall be like X.X.X.BUILD_NUMBER
-    build_number = version.split(".")[-1]
-    headers = {'Authorization': f"token {github_token}"}
-    release_info = get_release_info(repo, version)
+  repo = os.environ["GITHUB_REPOSITORY"]
+  organisation, project = repo.split("/")
+  tag = os.environ["GITHUB_REF"]
+  version = tag.replace('refs/tags/', '', 1)
+  # tag shall be like X.X.X.BUILD_NUMBER
+  build_number = version.split(".")[-1]
+  headers = {'Authorization': f"token {github_token}"}
+  release_info = get_release_info(repo, version)
 
-    if not release_info:
-        print(f"::error  No release info found")
-        return
+  if not release_info:
+    print(f"::error  No release info found")
+    return
 
-    try:
-        relesability.releasability_checks(project, version, current_branch())
-    except Exception as e:
-        print(f"::error relesability did not complete correctly. " + str(e))
-        print(traceback.format_exc())
-        sys.exit(1)
+  # try:
+  #     relesability.releasability_checks(project, version, current_branch())
+  # except Exception as e:
+  #     print(f"::error relesability did not complete correctly. " + str(e))
+  #     print(traceback.format_exc())
+  #     sys.exit(1)
 
-    artifactory = Artifactory(artifactory_apikey)
-    rr = ReleaseRequest(organisation, project, build_number)
+  artifactory = Artifactory(artifactory_apikey)
+  rr = ReleaseRequest(organisation, project, build_number)
 
-    try:
-        release.release(artifactory, rr, attach, run_rules_cov)
-        set_release_output("release", f"{repo}:{version} release DONE")
-        if distribute == 'true':
-            distributeStep.distribute_release(artifactory, rr)
-            set_release_output("distribute_release", f"{repo}:{version} distribute_release DONE")
-    except Exception as e:
-        print(f"::error release did not complete correctly." + str(e))
-        abort_release(repo, version)
-        sys.exit(1)
+  try:
+    release.release(artifactory, rr, attach, run_rules_cov)
+    set_release_output("release", f"{repo}:{version} release DONE")
+    if distribute == 'true':
+      distributeStep.distribute_release(artifactory, rr)
+      set_release_output("distribute_release", f"{repo}:{version} distribute_release DONE")
+  except Exception as e:
+    print(f"::error release did not complete correctly." + str(e))
+    abort_release(repo, version)
+    sys.exit(1)
 
 
 if __name__ == "__main__":
-    main()
+  main()
--- utils/artifactory.py
+++ utils/artifactory.py
@@ -1,10 +1,8 @@
 # repox
 import json
-import os
 import urllib
-import requests
-from main import artifactory_apikey
 
+import requests
 
 artifactory_url = 'https://repox.jfrog.io/repox'
 AUTHENTICATED = "authenticated"
@@ -13,141 +11,141 @@ bintray_target_repo = "SonarQube-bintray"
 
 
 class Artifactory:
+  api_key = None
+  headers = {'content-type': 'application/json'}
 
-    headers = {'content-type': 'application/json'}
+  def __init__(self, api_key: str):
+    self.api_key = api_key
+    self.headers['X-JFrog-Art-Api'] = api_key
 
-    def __init__(self, api_key: str):
-        self.api_key = api_key
-        self.headers['X-JFrog-Art-Api'] = api_key
+  def receive_build_info(self, release_request):
+    url = f"{artifactory_url}/api/build/{release_request.project}/{release_request.buildnumber}"
+    r = requests.get(url, headers=self.headers)
+    buildinfo = r.json()
+    if r.status_code == 200:
+      return BuildInfo(buildinfo)
+    else:
+      print(r.status_code)
+      print(r.content)
+      raise Exception('unknown build')
 
-    def receive_build_info(self, release_request):
-        url = f"{artifactory_url}/api/build/{release_request.project}/{release_request.buildnumber}"
-        r = requests.get(url, headers=self.headers)
-        buildinfo = r.json()
-        if r.status_code == 200:
-            return BuildInfo(buildinfo)
-        else:
-            print(r.status_code)
-            print(r.content)
-            raise Exception('unknown build')
-
-    def distribute_build(self, project, buildnumber):
-        print(f"Distributing {project}#{buildnumber} to bintray")
-        payload = {
-            "targetRepo": bintray_target_repo,
-            "sourceRepos": ["sonarsource-public-releases"],
-            "async": "true"  # maybe?
-        }
-        url = f"{artifactory_url}/api/build/distribute/{project}/{buildnumber}"
-        try:
-            r = requests.post(url, json=payload, headers=self.headers)
-            r.raise_for_status()
-            if r.status_code == 200:
-                print(
-                    f"{project}#{buildnumber} pushed to bintray ready to sync to central")
-        except requests.exceptions.HTTPError as err:
-            print(f"Failed to distribute {project}#{buildnumber} {err}")
-
-    def promote(self, release_request, buildinfo):
-        status = 'released'
-
-        repo = buildinfo.get_property('buildInfo.env.ARTIFACTORY_DEPLOY_REPO')
-        sourcerepo = repo.replace('qa', 'builds')
-        targetrepo = repo.replace('qa', 'releases')
-
+  def distribute_build(self, project, buildnumber):
+    print(f"Distributing {project}#{buildnumber} to bintray")
+    payload = {
+      "targetRepo": bintray_target_repo,
+      "sourceRepos": ["sonarsource-public-releases"],
+      "async": "true"  # maybe?
+    }
+    url = f"{artifactory_url}/api/build/distribute/{project}/{buildnumber}"
+    try:
+      r = requests.post(url, json=payload, headers=self.headers)
+      r.raise_for_status()
+      if r.status_code == 200:
         print(
-            f"Promoting build {release_request.project}#{release_request.buildnumber} from {sourcerepo} to {targetrepo}")
+          f"{project}#{buildnumber} pushed to bintray ready to sync to central")
+    except requests.exceptions.HTTPError as err:
+      print(f"Failed to distribute {project}#{buildnumber} {err}")
 
-        if buildinfo.is_multi():
-            print(f"Promoting multi repositories")
-            params = {
-                'buildName': release_request.project,
-                'buildNumber': release_request.buildnumber,
-                'src1': 'sonarsource - private - builds',
-                'target1': 'sonarsource - private - releases',
-                'src2': 'sonarsource - public - builds',
-                'target2': 'sonarsource - public - releases',
-                'status': status
-            }
-            url = f"{artifactory_url}/api/plugins/execute/multiRepoPromote?params=" + ";".join("{!s}={!r}".format(key, val) for (key, val) in params.items())
-            r = requests.get(url, headers=self.headers)
-        else:
-            url = f"{artifactory_url}/api/build/promote/{release_request.project}/{release_request.buildnumber}"
-            json_payload = {
-                "status": f"{status}",
-                "sourceRepo": f"{sourcerepo}",
-                "targetRepo": f"{targetrepo}"
-            }
-            r = requests.post(url, data=json.dumps(json_payload), headers=self.headers)
-        if not r.ok:
-            raise Exception(f"Promotion failed with code: {r.status_code}. Response was: {r.text}")
+  def promote(self, release_request, buildinfo):
+    status = 'released'
 
-    def download(self, artifactory_repo, gid, aid, qual, ext, version):
-        # download artifact
-        gid_path = gid.replace(".", "/")
-        if gid.startswith('com.'):
-            artifactory_repo = artifactory_repo.replace('public', 'private')
-            binaries_repo = COMMERCIAL_REPO
-        artifactory = artifactory_url + "/" + artifactory_repo
+    repo = buildinfo.get_property('buildInfo.env.ARTIFACTORY_DEPLOY_REPO')
+    sourcerepo = repo.replace('qa', 'builds')
+    targetrepo = repo.replace('qa', 'releases')
 
-        filename = f"{aid}-{version}.{ext}"
-        if qual:
-            filename = f"{aid}-{version}-{qual}.{ext}"
-        url = f"{artifactory}/{gid_path}/{aid}/{version}/{filename}"
-        print(url)
-        opener = urllib.request.build_opener()
-        opener.addheaders = [('X-JFrog-Art-Api', artifactory_apikey)]
-        urllib.request.install_opener(opener)
-        # for sonarqube rename artifact from sonar-application.zip to sonarqube.zip
-        if aid == "sonar-application":
-            filename = f"sonarqube-{version}.zip"
-            aid = "sonarqube"
-        tempfile = f"/tmp/{filename}"
-        urllib.request.urlretrieve(url, tempfile)
-        print(f'downloaded {tempfile}')
-        return tempfile
+    print(
+      f"Promoting build {release_request.project}#{release_request.buildnumber} from {sourcerepo} to {targetrepo}")
+
+    if buildinfo.is_multi():
+      print(f"Promoting multi repositories")
+      params = {
+        'buildName': release_request.project,
+        'buildNumber': release_request.buildnumber,
+        'src1': 'sonarsource - private - builds',
+        'target1': 'sonarsource - private - releases',
+        'src2': 'sonarsource - public - builds',
+        'target2': 'sonarsource - public - releases',
+        'status': status
+      }
+      url = f"{artifactory_url}/api/plugins/execute/multiRepoPromote?params=" + ";".join(
+        "{!s}={!r}".format(key, val) for (key, val) in params.items())
+      r = requests.get(url, headers=self.headers)
+    else:
+      url = f"{artifactory_url}/api/build/promote/{release_request.project}/{release_request.buildnumber}"
+      json_payload = {
+        "status": f"{status}",
+        "sourceRepo": f"{sourcerepo}",
+        "targetRepo": f"{targetrepo}"
+      }
+      r = requests.post(url, data=json.dumps(json_payload), headers=self.headers)
+    if not r.ok:
+      raise Exception(f"Promotion failed with code: {r.status_code}. Response was: {r.text}")
+
+  def download(self, artifactory_repo, gid, aid, qual, ext, version):
+    # download artifact
+    gid_path = gid.replace(".", "/")
+    if gid.startswith('com.'):
+      artifactory_repo = artifactory_repo.replace('public', 'private')
+      binaries_repo = COMMERCIAL_REPO
+    artifactory = artifactory_url + "/" + artifactory_repo
+
+    filename = f"{aid}-{version}.{ext}"
+    if qual:
+      filename = f"{aid}-{version}-{qual}.{ext}"
+    url = f"{artifactory}/{gid_path}/{aid}/{version}/{filename}"
+    print(url)
+    opener = urllib.request.build_opener()
+    opener.addheaders = [('X-JFrog-Art-Api', self.api_key)]
+    urllib.request.install_opener(opener)
+    # for sonarqube rename artifact from sonar-application.zip to sonarqube.zip
+    if aid == "sonar-application":
+      filename = f"sonarqube-{version}.zip"
+      aid = "sonarqube"
+    tempfile = f"/tmp/{filename}"
+    urllib.request.urlretrieve(url, tempfile)
+    print(f'downloaded {tempfile}')
+    return tempfile
 
 
 class BuildInfo:
+  json = None
 
-    json = None
+  def __init__(self, json):
+    self.json = json
 
-    def __init__(self, json):
-        self.json = json
+  def get_property(self, property, default=""):
+    try:
+      return self.json['buildInfo']['properties'][property]
+    except BaseException:
+      return default
 
-    def get_property(self, property, default=""):
-        try:
-            return self.json['buildInfo']['properties'][property]
-        except BaseException:
-            return default
+  def get_module_property(self, property):
+    return self.json['buildInfo']['modules'][0]['properties'][property]
 
-    def get_module_property(self, property):
-        return self.json['buildInfo']['modules'][0]['properties'][property]
+  def get_version(self):
+    return self.json['buildInfo']['modules'][0]['id'].split(":")[-1]
 
-    def get_version(self):
-        return self.json['buildInfo']['modules'][0]['id'].split(":")[-1]
+  def get_artifacts_to_publish(self):
+    artifacts = None
+    try:
+      artifacts = self.json.get_module_property('artifactsToPublish')
+    except:
+      try:
+        artifacts = self.json.get_property('buildInfo.env.ARTIFACTS_TO_PUBLISH')
+      except:
+        print("no artifacts to publish")
+    return artifacts
 
-    def get_artifacts_to_publish(self):
-        artifacts = None
-        try:
-            artifacts = self.json.get_module_property('artifactsToPublish')
-        except:
-            try:
-                artifacts = self.json.get_property('buildInfo.env.ARTIFACTS_TO_PUBLISH')
-            except:
-                print("no artifacts to publish")
-        return artifacts
-
-    def is_multi(self):
-        allartifacts = self.get_artifacts_to_publish()
-        if allartifacts:
-            artifacts = allartifacts.split(",")
-            artifacts_count = len(artifacts)
-            if artifacts_count == 1:
-                return False
-            ref = artifacts[0][0:3]
-            for i in range(0, artifacts_count):
-                current = artifacts[i - 1][0:3]
-                if current != ref:
-                    return True
+  def is_multi(self):
+    allartifacts = self.get_artifacts_to_publish()
+    if allartifacts:
+      artifacts = allartifacts.split(",")
+      artifacts_count = len(artifacts)
+      if artifacts_count == 1:
         return False
+      ref = artifacts[0][0:3]
+      for i in range(0, artifacts_count):
+        current = artifacts[i - 1][0:3]
+        if current != ref:
+          return True
+    return False
