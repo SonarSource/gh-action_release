3cc28a87810ca94f1daf509c8caf38d7f4a2797c
Malena Ebert <malena.ebert@sonarsource.com>
2020-07-20 11:15:17 +0200

    Use 2 spaces indentation

--- steps/distribute.py
+++ steps/distribute.py
@@ -1,29 +1,20 @@
+from utils import ReleaseRequest
 from utils.artifactory import Artifactory
 
 
-class ReleaseRequest:
-    def __init__(self, org, project, buildnumber):
-        self.org = org
-        self.project = project
-        self.buildnumber = buildnumber
-
-    def is_sonarqube(self):
-        return self.project == 'sonar-enterprise'
-
-
 def distribute_release(artifactory: Artifactory, release_request: ReleaseRequest):
-    buildinfo = artifactory.receive_build_info(release_request)
-    try:
-        if check_public(buildinfo):
-            artifactory.distribute_build(release_request.project, release_request.buildnumber)
-    except Exception as e:
-        print(f"Could not get repository for {release_request.project} {release_request.buildnumber} {str(e)}")
-        raise e
+  buildinfo = artifactory.receive_build_info(release_request)
+  try:
+    if check_public(buildinfo):
+      artifactory.distribute_build(release_request.project, release_request.buildnumber)
+  except Exception as e:
+    print(f"Could not get repository for {release_request.project} {release_request.buildnumber} {str(e)}")
+    raise e
 
 
 def check_public(buildinfo):
-    artifacts = buildinfo.get_artifacts_to_publish()
-    if artifacts:
-        return "org.sonarsource" in artifacts
-    else:
-        return False
+  artifacts = buildinfo.get_artifacts_to_publish()
+  if artifacts:
+    return "org.sonarsource" in artifacts
+  else:
+    return False
--- utils/binaries.py
+++ utils/binaries.py
@@ -13,47 +13,48 @@ binaries_url = f"https://{binaries_host}"
 ssh_user = 'ssuopsa'
 ssh_key = 'id_rsa_ssuopsa'
 
-def upload(tempfile, filename, aid, version):
-    binaries_repo = OSS_REPO
 
-    # upload artifact
-    ssh_client = paramiko.SSHClient()
-    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh_client.connect(hostname=binaries_host, username=ssh_user, key_filename=ssh_key)
-    # SonarLint Eclipse is uploaded to a special directory
-    if aid == "org.sonarlint.eclipse.site":
-        directory = f"{binaries_path_prefix}/SonarLint-for-Eclipse/releases"
-        release_url = f"{binaries_url}/SonarLint-for-Eclipse/releases/{filename}"
-    else:
-        directory = f"{binaries_path_prefix}/{binaries_repo}/{aid}"
-        release_url = f"{binaries_url}/{binaries_repo}/{aid}/{filename}"
-    # create directory
-    exec_ssh_command(ssh_client, f"mkdir -p {directory}")
-    print(f'created {directory}')
-    scp = SCPClient(ssh_client.get_transport())
-    print('scp connexion created')
-    # upload file to binaries
-    scp.put(tempfile, remote_path=directory)
-    print(f'uploaded {tempfile} to {directory}')
-    scp.close()
-    # SonarLint Eclipse is also unzipped on binaries for compatibility with P2 client
-    if aid == "org.sonarlint.eclipse.site":
-        sle_unzip_dir = f"{directory}/{version}"
-        exec_ssh_command(ssh_client, f"mkdir -p {sle_unzip_dir}")
-        exec_ssh_command(ssh_client, f"cd {sle_unzip_dir} && unzip ../org.sonarlint.eclipse.site-{version}.zip")
-    # sign file
-    exec_ssh_command(ssh_client,
-                     f"gpg --batch --passphrase {passphrase} --armor --detach-sig --default-key infra@sonarsource.com {directory}/{filename}")
-    print(f'signed {directory}/{filename}')
-    ssh_client.close()
-    return release_url
+def upload(tempfile, filename, aid, version):
+  binaries_repo = OSS_REPO
+
+  # upload artifact
+  ssh_client = paramiko.SSHClient()
+  ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+  ssh_client.connect(hostname=binaries_host, username=ssh_user, key_filename=ssh_key)
+  # SonarLint Eclipse is uploaded to a special directory
+  if aid == "org.sonarlint.eclipse.site":
+    directory = f"{binaries_path_prefix}/SonarLint-for-Eclipse/releases"
+    release_url = f"{binaries_url}/SonarLint-for-Eclipse/releases/{filename}"
+  else:
+    directory = f"{binaries_path_prefix}/{binaries_repo}/{aid}"
+    release_url = f"{binaries_url}/{binaries_repo}/{aid}/{filename}"
+  # create directory
+  exec_ssh_command(ssh_client, f"mkdir -p {directory}")
+  print(f'created {directory}')
+  scp = SCPClient(ssh_client.get_transport())
+  print('scp connexion created')
+  # upload file to binaries
+  scp.put(tempfile, remote_path=directory)
+  print(f'uploaded {tempfile} to {directory}')
+  scp.close()
+  # SonarLint Eclipse is also unzipped on binaries for compatibility with P2 client
+  if aid == "org.sonarlint.eclipse.site":
+    sle_unzip_dir = f"{directory}/{version}"
+    exec_ssh_command(ssh_client, f"mkdir -p {sle_unzip_dir}")
+    exec_ssh_command(ssh_client, f"cd {sle_unzip_dir} && unzip ../org.sonarlint.eclipse.site-{version}.zip")
+  # sign file
+  exec_ssh_command(ssh_client,
+                   f"gpg --batch --passphrase {passphrase} --armor --detach-sig --default-key infra@sonarsource.com {directory}/{filename}")
+  print(f'signed {directory}/{filename}')
+  ssh_client.close()
+  return release_url
 
 
 def exec_ssh_command(ssh_client, command):
-    stdin, stdout, stderr = ssh_client.exec_command(command)
-    stdout_contents = '\n'.join(stdout.readlines())
-    print(f"stdout: {stdout_contents}")
-    stderr_contents = '\n'.join(stderr.readlines())
-    print(f"stderr: {stderr_contents}")
-    if stderr_contents:
-        raise Exception(f"Error during the SSH command '{command}': {stderr_contents}")
+  stdin, stdout, stderr = ssh_client.exec_command(command)
+  stdout_contents = '\n'.join(stdout.readlines())
+  print(f"stdout: {stdout_contents}")
+  stderr_contents = '\n'.join(stderr.readlines())
+  print(f"stderr: {stderr_contents}")
+  if stderr_contents:
+    raise Exception(f"Error during the SSH command '{command}': {stderr_contents}")
