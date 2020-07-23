087592c81c09738a91207174a0a85cedcd69c0f9
Tom <thomas.verin@gmail.com>
2020-07-23 15:16:50 +0200

    Fix release revoke on binaries

--- steps/release.py
+++ steps/release.py
@@ -33,32 +33,37 @@ def revoke_release(artifactory: Artifactory, binaries, release_request: ReleaseR
   try:
     publish_all_artifacts(artifactory, binaries, release_request, buildinfo, revoke)
   except Exception as e:
-    print(f"Error could not unpromote {release_request.project} {release_request.buildnumber} {str(e)}")
+    print(f"Error could not delete {release_request.project} {release_request.buildnumber} {str(e)}")
     raise e
 
+def get_action(revoke):
+  if revoke:
+    return "deleting"
+  else:
+    return "publishing"
 
 def publish_all_artifacts(artifactory, binaries, release_request, buildinfo, revoke=False):
-  print(f"publishing artifacts for {release_request.project}#{release_request.buildnumber}")
+  print(f"{get_action(revoke)} artifacts for {release_request.project}#{release_request.buildnumber}")
   release_url = ""
   repo = buildinfo.get_property('buildInfo.env.ARTIFACTORY_DEPLOY_REPO').replace('qa', 'builds')
   version = buildinfo.get_version()
   allartifacts = buildinfo.get_artifacts_to_publish()
   if allartifacts:
-    print(f"publishing: {allartifacts}")
+    print(f"{get_action(revoke)}: {allartifacts}")
     artifacts = allartifacts.split(",")
     artifacts_count = len(artifacts)
     if artifacts_count == 1:
       print("only 1")
-      return publish_artifact(artifactory, binaries, artifacts[0], version, repo)
+      return publish_artifact(artifactory, binaries, artifacts[0], version, repo, revoke)
     print(f"{artifacts_count} artifacts")
     for i in range(0, artifacts_count):
       print(f"artifact {artifacts[i]}")
-      release_url = publish_artifact(artifactory, binaries, artifacts[i], version, repo)
+      release_url = publish_artifact(artifactory, binaries, artifacts[i], version, repo, revoke)
   return release_url
 
 
 def publish_artifact(artifactory, binaries, artifact_to_publish, version, repo, revoke=False):
-  print(f"publishing {artifact_to_publish}#{version}")
+  print(f"{get_action(revoke)} {artifact_to_publish}#{version}")
   artifact = artifact_to_publish.split(":")
   gid = artifact[0]
   aid = artifact[1]
@@ -72,8 +77,8 @@ def publish_artifact(artifactory, binaries, artifact_to_publish, version, repo, 
   filename = f"{aid}-{version}.{ext}"
   if qual:
     filename = f"{aid}-{version}-{qual}.{ext}"
-  tempfile = artifactory.download(artifactory_repo, gid, aid, qual, ext, version)
   if revoke:
-    binaries.delete(tempfile, filename, gid, aid, version)
-  else:
+    binaries.delete(filename, gid, aid, version)
+  else: 
+    tempfile = artifactory.download(artifactory_repo, gid, aid, qual, ext, version)
     return binaries.upload(tempfile, filename, gid, aid, version)
