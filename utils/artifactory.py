ec91797374ecf49951a67f77aa48555ae91c6836
Malena Ebert <malena.ebert@sonarsource.com>
2020-08-07 12:00:36 +0200

    Move BuildInfo class to seperate file

--- utils/artifactory.py
+++ utils/artifactory.py
@@ -2,6 +2,8 @@ import json
 import urllib
 import requests
 
+from utils.buildinfo import BuildInfo
+
 
 class Artifactory:
 
@@ -118,47 +120,3 @@ class Artifactory:
       urllib.request.urlretrieve(f"{url}.{checksum}", f"{tempfile}.{checksum}")
       print(f'downloaded {tempfile}.{checksum}')
     return tempfile
-
-
-class BuildInfo:
-  json = None
-
-  def __init__(self, json):
-    self.json = json
-
-  def get_property(self, property, default=""):
-    try:
-      return self.json['buildInfo']['properties'][property]
-    except BaseException:
-      return default
-
-  def get_module_property(self, property):
-    return self.json['buildInfo']['modules'][0]['properties'][property]
-
-  def get_version(self):
-    return self.json['buildInfo']['modules'][0]['id'].split(":")[-1]
-
-  def get_artifacts_to_publish(self):
-    artifacts = None
-    try:
-      artifacts = self.get_module_property('artifactsToPublish')
-    except:
-      try:
-        artifacts = self.get_property('buildInfo.env.ARTIFACTS_TO_PUBLISH')
-      except:
-        print("no artifacts to publish")
-    return artifacts
-
-  def is_multi(self):
-    allartifacts = self.get_artifacts_to_publish()
-    if allartifacts:
-      artifacts = allartifacts.split(",")
-      artifacts_count = len(artifacts)
-      if artifacts_count == 1:
-        return False
-      ref = artifacts[0][0:3]
-      for i in range(0, artifacts_count):
-        current = artifacts[i - 1][0:3]
-        if current != ref:
-          return True
-    return False
