e99f7f3e04570b1c1ec12681f0394052cf69ed46
Tom <thomas.verin@sonarsource.com>
2020-08-26 08:47:04 +0200

    Show payload

--- utils/artifactory.py
+++ utils/artifactory.py
@@ -42,6 +42,7 @@ class Artifactory:
       "sourceRepos": [source_repo],
       "async": "false"
     }
+    print(f"payload: {payload}")
     url = f"{self.url}/api/build/distribute/{release_request.project}/{release_request.buildnumber}"
     try:
       r = requests.post(url, json=payload, headers=self.headers)
