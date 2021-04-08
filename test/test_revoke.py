8e191bb2713e397bf2e70832a7e16134ac6828f1
Jean-Baptiste Lievremont <jeanbaptiste.lievremont@sonarsource.com>
2021-04-08 17:55:58 +0200

    Make sure that relevant tests pass without the full environment

--- test/test_revoke.py
+++ test/test_revoke.py
@@ -1,15 +1,10 @@
-import os
 import unittest
 
 from utils.artifactory import Artifactory
 from utils.binaries import Binaries
 from utils.ReleaseRequest import ReleaseRequest
-from steps.release import revoke_release, publish_all_artifacts_to_binaries
 
-from vars import githup_api_url, github_token, github_event_path, burgrx_url, burgrx_user, burgrx_password, \
-  artifactory_apikey, distribute_target, bintray_api_url, bintray_user, bintray_apikey, central_user, central_password, \
-  binaries_ssh_key, binaries_host, binaries_ssh_user, binaries_path_prefix, passphrase, run_rules_cov, distribute, repo, ref, publish_to_binaries, \
-  slack_client
+from vars import artifactory_apikey, binaries_ssh_key, binaries_host, binaries_ssh_user, binaries_path_prefix, passphrase
 
 
 class TestRevoke(unittest.TestCase):
@@ -17,7 +12,7 @@ class TestRevoke(unittest.TestCase):
   def setUp(self) -> None:
     self.sonar_dummy_request = ReleaseRequest('SonarSource', 'sonar-dummy', '460')
 
-
+  @unittest.skipIf(binaries_ssh_key == 'no ssh key in env', 'No SSH key in environment')
   def test_revoke_release(self):
     artifactory = Artifactory(artifactory_apikey)
     binaries = Binaries(binaries_host, binaries_ssh_user, binaries_ssh_key, binaries_path_prefix, passphrase)
