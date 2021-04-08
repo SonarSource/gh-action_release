d8015254ac36767eba598f12c2351bdd68236f71
Jean-Baptiste Lievremont <jeanbaptiste.lievremont@sonarsource.com>
2021-04-08 17:55:58 +0200

    Make sure that relevant tests pass without the full environment

--- test/test_slack.py
+++ test/test_slack.py
@@ -1,18 +1,27 @@
 import unittest
+from unittest.mock import Mock
 from utils.bintray import Bintray
-from slack.errors import SlackApiError
+from vars import bintray_api_url, bintray_user, bintray_apikey, central_user, central_password
 
-from vars import githup_api_url, github_token, github_event_path, burgrx_url, burgrx_user, burgrx_password, \
-  artifactory_apikey, distribute_target, bintray_api_url, bintray_user, bintray_apikey, central_user, central_password, \
-  binaries_ssh_key, binaries_host, binaries_ssh_user, binaries_path_prefix, passphrase, run_rules_cov, distribute, repo, ref, publish_to_binaries, \
-  slack_client
+def mock_chat_post_message(channel=None, text=None):
+  return {
+    'message': {
+      'text': text
+    }
+  }
+
+slack_client = Mock()
+slack_client.chat_postMessage = Mock(side_effect = mock_chat_post_message)
 
 class TestSlack(unittest.TestCase):
 
   def test_slack(self):
+    message = "test message"
+    channel = "#test-github-action"
     bintray = Bintray(bintray_api_url, bintray_user, bintray_apikey, central_user, central_password, slack_client)
-    response = bintray.alert_slack("test message","#test-github-action")    
-    assert response["message"]["text"] == "test message"
+    response = bintray.alert_slack(message, channel)
+    assert response["message"]["text"] == message
+    slack_client.chat_postMessage.assert_called_with(channel=channel, text=message)
 
 if __name__ == '__main__':
     unittest.main()
