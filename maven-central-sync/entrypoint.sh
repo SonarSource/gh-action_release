9baf515f9541a39d0f78e2c27bbb68ffeb4984f5
Malena Ebert <63863184+malena-ebert-sonarsource@users.noreply.github.com>
2021-04-16 12:13:17 +0200

    changes due to the review

--- maven-central-sync/entrypoint.sh
+++ maven-central-sync/entrypoint.sh
@@ -4,15 +4,20 @@ set -euo pipefail
 
 LOCAL_REPO_DIR=$1
 
-CMD="mvn org.sonatype.plugins:nexus-staging-maven-plugin:1.6.7:"
+MVN_NEXUS_STAGING_CMD="mvn org.sonatype.plugins:nexus-staging-maven-plugin:1.6.7:"
 DEFAULT_OPTS="-DnexusUrl=https://s01.oss.sonatype.org/ -DserverId=ossrh"
 PROFILE_OPT="-DstagingProfileId=13c1877339a4cf"
 
-open="$CMD:rc-open $DEFAULT_OPTS $PROFILE_OPT"
-deploy="$CMD:deploy-staged-repository $DEFAULT_OPTS $PROFILE_OPT"
-close="$CMD:rc-close $DEFAULT_OPTS"
-release="$CMD:rc-release $DEFAULT_OPTS"
+# Open a new staging repository
+open="$MVN_NEXUS_STAGING_CMD:rc-open $DEFAULT_OPTS $PROFILE_OPT"
+# Deploy to the staging repository
+deploy="$MVN_NEXUS_STAGING_CMD:deploy-staged-repository $DEFAULT_OPTS $PROFILE_OPT"
+# Close the staging repository after deployment and perform checks
+close="$MVN_NEXUS_STAGING_CMD:rc-close $DEFAULT_OPTS"
+# Release the artifacts to the public repository
+release="$MVN_NEXUS_STAGING_CMD:rc-release $DEFAULT_OPTS"
 
+# R3P0 is a magic string to ensure that the correct line is grepped
 STAGING_REPO=$($open -DopenedRepositoryMessageFormat="R3P0:%s" | grep "R3P0:" | cut -d: -f2)
 
 echo "::set-output name=repo=${STAGING_REPO}"
