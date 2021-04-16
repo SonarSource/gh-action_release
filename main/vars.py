0a4651237c4b2c5df51831394de64120adedd523
Malena Ebert <63863184+malena-ebert-sonarsource@users.noreply.github.com>
2021-04-16 11:33:31 +0200

    Remove Bintray support

--- main/vars.py
+++ main/vars.py
@@ -19,13 +19,6 @@ distribute: bool = os.environ.get('INPUT_DISTRIBUTE', 'false').lower() == "true"
 run_rules_cov: bool = os.environ.get('INPUT_RUN_RULES_COV', 'false').lower() == "true"
 
 artifactory_apikey = os.environ.get('ARTIFACTORY_API_KEY', 'no api key in env')
-distribute_target = os.environ.get('INPUT_DISTRIBUTE_TARGET') or None
-
-bintray_api_url='https://api.bintray.com'
-bintray_user=os.environ.get('BINTRAY_USER','no bintray api user in env')
-bintray_apikey=os.environ.get('BINTRAY_TOKEN','no bintray api key in env')
-central_user=os.environ.get('CENTRAL_USER','no central user in env')
-central_password=os.environ.get('CENTRAL_PASSWORD','no central password in env')
 
 binaries_host = 'binaries.sonarsource.com'
 binaries_ssh_user=os.environ.get('RELEASE_SSH_USER','no ssh user in env')
