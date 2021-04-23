be32fe47964ed360913e7d26642d3a3c95d269ce
Malena Ebert <63863184+malena-ebert-sonarsource@users.noreply.github.com>
2021-04-23 21:38:30 +0200

    Fail on revoked key (#23)
    * Fail on revoked sign key
    
    * Print build
    
    * Fix repo

---
+++ main/bin/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -oue pipefail
+
+REVOKED_KEY=CFCA4A29D26468DE
+BUILD="${GITHUB_REPOSITORY#*/}/${GITHUB_REF##*.}"
+
+echo "BUILD=$BUILD"
+
+jfrog rt config repox --url https://repox.jfrog.io/artifactory/ --apikey "$ARTIFACTORY_API_KEY" --basic-auth-only
+cd "$(mktemp -d sigcheck.XXXXXXXXXX)" && {
+  jfrog rt download --fail-no-op --build "${BUILD}" sonarsource-builds/**/*.asc
+  find . -type f -name "*.asc" -exec gpg --list-packets {} \; | grep ${REVOKED_KEY} && exit 1 || true
+}
+
+exec python /app/main.py
