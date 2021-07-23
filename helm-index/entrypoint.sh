a5e794bf9f6f05203025b8227efa0af7f779d8c6
Malena Ebert <63863184+malena-ebert-sonarsource@users.noreply.github.com>
2021-07-23 15:21:17 +0200

    fix repo name

--- helm-index/entrypoint.sh
+++ helm-index/entrypoint.sh
@@ -7,7 +7,7 @@ mkdir -p .cr-index
 
 cr index \
     --charts-repo "$GITHUB_SERVER_URL/$GITHUB_REPOSITORY" \
-    --git-repo "$GITHUB_REPOSITORY" \
+    --git-repo "$GITHUB_REPOSITORY_SHORT" \
     --owner "SonarSource" \
     --package-path "$1" \
     --release-name-template "$2" \
