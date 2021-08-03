bd7d1b6c2cf86e45cb0ce3c40ddffe4820189c25
Malena Ebert <63863184+malena-ebert-sonarsource@users.noreply.github.com>
2021-08-03 15:05:40 +0200

    Fix extension

--- helm-index/entrypoint.sh
+++ helm-index/entrypoint.sh
@@ -4,9 +4,9 @@ set -euo pipefail
 
 GH_PAGES_FOLDER="$(pwd)/../gh-pages"
 git worktree add "$GH_PAGES_FOLDER" gh-pages
-helm repo index --merge "$GH_PAGES_FOLDER/index.ymal" "$1"
+helm repo index --merge "$GH_PAGES_FOLDER/index.yaml" "$1"
 (cd "$GH_PAGES_FOLDER" && {
-    git add index.ymal
+    git add index.yaml
     git commit --message "Update index"
     git push
 })
