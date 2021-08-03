839f698e899d7c452651a53b3ae5bdce548ff21d
Malena Ebert <63863184+malena-ebert-sonarsource@users.noreply.github.com>
2021-08-03 15:03:19 +0200

    Fix index cmd call

--- helm-index/entrypoint.sh
+++ helm-index/entrypoint.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 GH_PAGES_FOLDER="$(pwd)/../gh-pages"
 git worktree add "$GH_PAGES_FOLDER" gh-pages
-helm index --merge "$GH_PAGES_FOLDER/index.ymal" "$1"
+helm repo index --merge "$GH_PAGES_FOLDER/index.ymal" "$1"
 (cd $GH_PAGES_FOLDER && {
     git add index.ymal
     git commit --message "Update index"
