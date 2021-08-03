fba9e75c20111c5eb66661720c7abb4e079c7eb6
Malena Ebert <63863184+malena-ebert-sonarsource@users.noreply.github.com>
2021-08-03 15:07:58 +0200

    Print charts dir

--- helm-index/entrypoint.sh
+++ helm-index/entrypoint.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 
 GH_PAGES_FOLDER="$(pwd)/../gh-pages"
 git worktree add "$GH_PAGES_FOLDER" gh-pages
+ls -l "$1"
 helm repo index --merge "$GH_PAGES_FOLDER/index.yaml" "$1"
 (cd "$GH_PAGES_FOLDER" && {
     git add index.yaml
