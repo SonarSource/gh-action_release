717f754cb91084799a0071a7f0d8116b746346b6
Malena Ebert <63863184+malena-ebert-sonarsource@users.noreply.github.com>
2021-08-03 15:24:07 +0200

    Love you helm!!!!

--- helm-index/entrypoint.sh
+++ helm-index/entrypoint.sh
@@ -6,9 +6,9 @@ set -x
 
 GH_PAGES_FOLDER="$(pwd)/../gh-pages"
 git worktree add "$GH_PAGES_FOLDER" gh-pages
-ls -l "$1"
-helm repo index --merge "$GH_PAGES_FOLDER/index.yaml" "$1"
+cp "$1/*.tgz" "$GH_PAGES_FOLDER"
 (cd "$GH_PAGES_FOLDER" && {
+    helm repo index --merge index.yaml .
     git add index.yaml
     git commit --message "Update index"
     git push
