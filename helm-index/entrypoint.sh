33a6bdd6d21e85cc9def6102002b0141761ec9b0
Malena Ebert <63863184+malena-ebert-sonarsource@users.noreply.github.com>
2021-08-03 15:36:26 +0200

    Add url flag

--- helm-index/entrypoint.sh
+++ helm-index/entrypoint.sh
@@ -8,7 +8,7 @@ GH_PAGES_FOLDER="$(pwd)/../gh-pages"
 git worktree add "$GH_PAGES_FOLDER" gh-pages
 cp -r "$1/" "$GH_PAGES_FOLDER/"
 (cd "$GH_PAGES_FOLDER" && {
-    helm repo index --merge index.yaml .
+    helm repo index --url "https://github.com/SonarSource/$GITHUB_REPOSITORY/releases/download/$2/" --merge index.yaml .
     git add index.yaml
     git commit --message "Update index"
     git push
