860218acae1cd31a0506c57441a60bf99b47666e
Malena Ebert <63863184+malena-ebert-sonarsource@users.noreply.github.com>
2021-08-03 14:58:40 +0200

    Use helm --index and git

--- helm-index/entrypoint.sh
+++ helm-index/entrypoint.sh
@@ -2,16 +2,11 @@
 
 set -euo pipefail
 
-echo $@
-
-rm -rf .cr-index
-mkdir -p .cr-index
-
-cr index \
-    --charts-repo "$GITHUB_SERVER_URL/$GITHUB_REPOSITORY" \
-    --git-repo "$GITHUB_REPOSITORY_SHORT" \
-    --owner "SonarSource" \
-    --package-path "$1" \
-    --release-name-template "$2" \
-    --push \
-    --token $CR_TOKEN
+GH_PAGES_FOLDER="$(pwd)/../gh-pages"
+git worktree add "$GH_PAGES_FOLDER" gh-pages
+helm index --merge "$GH_PAGES_FOLDER/index.ymal" "$1"
+(cd $GH_PAGES_FOLDER && {
+    git add index.ymal
+    git commit --message "Update index"
+    git push
+})
