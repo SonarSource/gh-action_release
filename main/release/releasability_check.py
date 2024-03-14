75288249f2b15b83837b20091010e4b34c311b62
Mate Molnar <matemoln@users.noreply.github.com>
2024-03-14 16:12:05 +0100

    Add root directory to PYTHONPATH in releasability_check.py

--- main/release/releasability_check.py
+++ main/release/releasability_check.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from release.utils.burgr import Burgr
 from release.utils.github import GitHub
 from release.utils.releasability import Releasability
