0f00002c7187710b8f0dbc4a091578d1461feee5
Mate Molnar <matemoln@users.noreply.github.com>
2024-03-19 14:11:22 +0100

    Reference SNS topics and SQS queue directly instead of using SSM parameters

--- main/release/utils/releasability.py
+++ main/release/utils/releasability.py
@@ -2,16 +2,9 @@ import uuid
 import boto3
 import json
 from boto3 import Session
-from dryable import Dryable
 from release.steps.ReleaseRequest import ReleaseRequest
-from release.utils.aws_ssm_parameter_helper import AwsSsmParameterHelper
 from release.utils.version_helper import VersionHelper
-from release.vars import releasability_aws_region, releasability_env_type
-
-RELEASABILITY_SSM_PARAMETER_NAME_PREFIX = "/Burgr/Releasability/"
-RELEASABILITY_SSM_PARAMETER_NAME_TRIGGER_TOPIC_SUFFIX = "/ReleasabilityTriggerArn"
-RELEASABILITY_SSM_PARAMETER_NAME_RESULT_TOPIC_SUFFIX = "/ReleasabilityResultArn"
-RELEASABILITY_SSM_PARAMETER_NAME_RESULT_QUEUE_SUFFIX = "/ReleasabilityResultQueueArn"
+from release.vars import releasability_aws_region
 
 class ReleasabilityException(Exception):
     pass
@@ -64,31 +57,13 @@ class Releasability:
     def __init__(self, release_request):
         self.release_request = release_request
         self.session = boto3.Session(region_name=releasability_aws_region)
+        account_id = boto3.client('sts').get_caller_identity().get('Account')
 
-        self.TRIGGER_TOPIC_ARN = self._get_trigger_topic_arn(releasability_env_type)
-        self.RESULT_TOPIC_ARN = self._get_result_topic_arn(releasability_env_type)
-        self.RESULT_QUEUE_ARN = self._get_result_queue_arn(releasability_env_type)
+        self.TRIGGER_TOPIC_ARN = f"arn:aws:sns:{releasability_aws_region}:{account_id}:ReleasabilityTriggerTopic"
+        self.RESULT_TOPIC_ARN = f"arn:aws:sns:{releasability_aws_region}:{account_id}:ReleasabilityResultTopic"
+        self.RESULT_QUEUE_ARN = f"arn:aws:sqs:{releasability_aws_region}:{account_id}:ReleasabilityResultQueue"
 
-    @Dryable(logging_msg="{function}()")
-    def _get_trigger_topic_arn(self, env_type: str):
-        sns_input_arn_parameter_name = f"{RELEASABILITY_SSM_PARAMETER_NAME_PREFIX}{env_type}{RELEASABILITY_SSM_PARAMETER_NAME_TRIGGER_TOPIC_SUFFIX}"
-        return AwsSsmParameterHelper.get_ssm_parameter_value(
-            self.session, sns_input_arn_parameter_name
-        )
 
-    @Dryable(logging_msg="{function}()")
-    def _get_result_topic_arn(self, env_type: str):
-        sns_output_arn_parameter_name = f"{RELEASABILITY_SSM_PARAMETER_NAME_PREFIX}{env_type}{RELEASABILITY_SSM_PARAMETER_NAME_RESULT_TOPIC_SUFFIX}"
-        return AwsSsmParameterHelper.get_ssm_parameter_value(
-            self.session, sns_output_arn_parameter_name
-        )
-
-    @Dryable(logging_msg="{function}()")
-    def _get_result_queue_arn(self, env_type: str):
-        sns_output_arn_parameter_name = f"{RELEASABILITY_SSM_PARAMETER_NAME_PREFIX}{env_type}{RELEASABILITY_SSM_PARAMETER_NAME_RESULT_QUEUE_SUFFIX}"
-        return AwsSsmParameterHelper.get_ssm_parameter_value(
-            self.session, sns_output_arn_parameter_name
-        )
     def start_releasability_checks(self):
         standardized_version = VersionHelper.as_standardized_version(self.release_request)
 
