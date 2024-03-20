914ff9aa6c9901cf15329024c8da6e2680c866bd
Sebastien Vermeille <sebastien.vermeille@sonarsource.com>
2024-03-20 11:46:03 +0100

    Remove undesired change

--- main/release/releasability_check.py
+++ main/release/releasability_check.py
@@ -1,8 +1,3 @@
-import sys
-import os
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 from release.utils.burgr import Burgr
 from release.utils.github import GitHub
 from release.utils.releasability import Releasability
