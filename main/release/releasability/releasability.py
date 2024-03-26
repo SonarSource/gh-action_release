f22023126e37a3c8a5e548ad4b44f258fa6bbc4b
Sebastien Vermeille <sebastien.vermeille@sonarsource.com>
2024-03-26 13:55:15 +0100

    Use a static method

--- main/release/releasability/releasability.py
+++ main/release/releasability/releasability.py
@@ -148,7 +148,8 @@ class Releasability:
                 f'Received {received_message_count}/{expected_message_count} check result messages within '
                 f'allowed time ({Releasability.FETCH_CHECK_RESULT_TIMEOUT_SECONDS} seconds)')
 
-    def _build_filters(self, correlation_id: str) -> []:
+    @staticmethod
+    def _build_filters(correlation_id: str) -> []:
         def match_correlation_id(msg):
             return msg['requestUUID'] == correlation_id
 
