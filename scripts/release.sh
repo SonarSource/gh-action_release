15a88df324f385f9bb0c8d3b7445cbf0a699bc9c
Julien Carsique <julien.carsique@sonarsource.com>
2024-04-03 19:37:06 +0200

    feat(BUILD-4729): wip

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Release a new version of the action
 # Usage: scripts/release.sh <branch> <version>
-set -xeuo pipefail
+set -euo pipefail
 
 type git gh >/dev/null
 branch=$1
@@ -24,13 +24,20 @@ git push origin "$version"
 gh pr create --base "${branch}" --title "Release $version" --body-file scripts/pull-request-body.txt -a @me --label auto-approve
 echo "Wait for PR approval..."
 counter=0
-while ! gh pr view --json state --jq .state | grep -q MERGED; do
-  ((counter++)) && ((counter == 30)) && exit 1
+while gh pr view --json state --jq .state | grep -q OPEN && ! gh pr view --json reviewDecision --jq .reviewDecision | grep -q APPROVED; do
+  ((counter++)) && ((counter == 30)) && echo "Timed out waiting for PR approval!" >&2 && exit 1
   printf "."
   sleep 5
 done
-git fetch
-git checkout "${branch}"
-git merge --ff-only "$working_branch"
-git push origin "${branch}"
+if gh pr view --json state --jq .state | grep -q OPEN && gh pr view --json reviewDecision --jq .reviewDecision | grep -q APPROVED; then
+  echo "Fast-forward merge approved PR..."
+  git fetch
+  git checkout "${branch}"
+  git merge --ff-only "$working_branch"
+  git push origin "${branch}"
+fi
+if ! gh pr view --json state --jq .state | grep -q MERGED; then
+  echo "PR is not merged, exit" >&2
+  exit 1
+fi
 gh release create "$version" -t "$version" --target "$(git show -s --pretty=format:'%H' "$version")" --verify-tag --generate-notes
