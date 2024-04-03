19d0a79a13ce8e5f08d904158942564429dad2ce
Julien Carsique <julien.carsique@sonarsource.com>
2024-04-03 19:41:01 +0200

    feat(BUILD-4729): release workflow

--- scripts/updatevbranch.sh
+++ scripts/updatevbranch.sh
@@ -4,7 +4,12 @@
 set -xeuo pipefail
 
 version=$1
-branch="v${version%%.*}"
-git fetch --tags
-git update-ref -m "reset: update branch $branch to tag $version" "refs/heads/$branch" "$version"
-git push origin "$branch:refs/heads/$branch"
+vbranch="v${version%%.*}"
+git fetch --tags --all
+git update-ref -m "reset: update branch $vbranch to tag $version" "refs/heads/$vbranch" "$version"
+git push origin "$vbranch:refs/heads/$vbranch" || (
+  git show -s --pretty=format:'%h%d' "$vbranch" "origin/$vbranch" "$version"
+  git log --graph --pretty=format:'%h -%d %s' --abbrev-commit "$vbranch" "origin/${vbranch}" "$version" -20
+  echo "Push failed, please check the output above" >&2
+  exit 1
+)
