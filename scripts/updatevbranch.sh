15c8297fcca3203b955fcbbd2a75e7953824953c
sonartech <sonartech@sonarsource.com>
2024-04-05 14:02:33 +0200

    feat(BUILD-4729): wip

--- scripts/updatevbranch.sh
+++ scripts/updatevbranch.sh
@@ -5,7 +5,7 @@ set -xeuo pipefail
 
 version=$1
 branch="v${version%%.*}"
-git fetch origin "$branch" "$version"
+git fetch --tags origin "$branch" "$version"
 git update-ref -m "reset: update branch $branch to tag $version" "refs/heads/$branch" "$version"
 git push origin "$branch:refs/heads/$branch" || (
   git push --dry-run origin "$branch:refs/heads/$branch"
