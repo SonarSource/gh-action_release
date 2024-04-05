02e91640ebb61126126f60912743149bca205ee9
sonartech <sonartech@sonarsource.com>
2024-04-05 18:32:19 +0200

    feat(BUILD-4729): wip vbranch

--- scripts/updatevbranch.sh
+++ scripts/updatevbranch.sh
@@ -6,12 +6,11 @@ set -xeuo pipefail
 version=$1
 vbranch="v${version%%.*}"
 git fetch --tags --all
-git checkout "$vbranch"
-git pull origin "$vbranch"
+git checkout -f "$vbranch"
 git update-ref -m "reset: update branch $vbranch to tag $version" "refs/heads/$vbranch" "$version"
 git push origin "$vbranch" || (
   git show -s --pretty=format:'%h%d' "$vbranch" "origin/$vbranch" "$version"
-  git log --graph --pretty=format:'%h -%d %s' --abbrev-commit "$vbranch...origin/${vbranch}~"
+  git log --graph --pretty=format:'%h -%d %s' --abbrev-commit "$vbranch" "origin/${vbranch}" "$version" -20
   echo "Push failed, please check the output above" >&2
   exit 1
 )
