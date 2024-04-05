e54878abbfd4f519a58006d342c2e111ae40e6bd
sonartech <sonartech@sonarsource.com>
2024-04-05 16:24:11 +0200

    feat(BUILD-4729): wip

--- scripts/updatevbranch.sh
+++ scripts/updatevbranch.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 # Update the v* branch to the tag version
-# Usage: scripts/updatevbranch.sh <version>
+# Usage: scripts/updatevbranch.sh <branch> <version>
 set -xeuo pipefail
 
-version=$1
-branch="v${version%%.*}"
-git fetch --tags origin "$branch"
-git update-ref -m "reset: update branch $branch to tag $version" "refs/heads/$branch" "$version"
-git push origin "$branch:refs/heads/$branch" || (
-  git show -s --pretty=format:'%h%d' "$branch" "origin/$branch" "$version"
-  git log --graph --pretty=format:'%h -%d %s' --abbrev-commit "$branch...origin/${branch}~"
+branch=$1
+version=$2
+vbranch="v${version%%.*}"
+git fetch --tags origin "$vbranch" "$branch"
+git update-ref -m "reset: update branch $vbranch to tag $version" "refs/heads/$vbranch" "$version"
+git push origin "$vbranch:refs/heads/$vbranch" || (
+  git show -s --pretty=format:'%h%d' "$vbranch" "origin/$vbranch" "$version"
+  git log --graph --pretty=format:'%h -%d %s' --abbrev-commit "$vbranch...origin/${vbranch}~"
   echo "Push failed, please check the output above" >&2
+  exit 1
 )
